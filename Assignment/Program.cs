0283ea660e420401a9efac0cbd7c76c631969718
AbdallahSay <luka3122003@gmail.com>
2025-07-26 06:50:52 +0300

    Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,9 +1,40 @@
 ﻿using Assignment.Q1;
+using Assignment.Q1.Q2;
+using System.Collections;
 
 namespace Assignment
 {
     internal class Program
     {
-        
+
+        public static void Main(string[] args)
+        {
+            #region Q2
+            //ArrayList items = new ArrayList() { "A", "B", "C", "D", "E" };
+
+            //Console.WriteLine("Before Reversal:");
+            //foreach (var item in items)
+            //    Console.Write(item + " ");
+            //Console.WriteLine();
+
+            //ArrayListUtils.ReverseArrayList(items);
+
+            //Console.WriteLine("After Reversal:");
+            //foreach (var item in items)
+            //    Console.Write(item + " ");
+
+            #endregion
+        }
+
     }
+   
+
+
+    
+      
+
+      
+
+  
 }
+
---
+++ Assignment/Q1/Q2/ArrayListUtils.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace Assignment.Q1.Q2
+{
+    internal class ArrayListUtils
+    {
+        public static void ReverseArrayList(ArrayList list)
+        {
+            int left = 0;
+            int right = list.Count - 1;
+            while(left < right)
+            {
+                object? temp = list[left];
+                list[left] = list[right];
+                list[right] = temp;
+
+                left++;
+                right--;
+
+            }
+
+        }
+    }
+}
